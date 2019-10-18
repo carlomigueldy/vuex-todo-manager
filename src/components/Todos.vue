f7f32a5056c991eb7a65d85215b0361b822bc903
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-18 20:37:03 +0800

    Brad is great

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
+
     <div class="todos">
       <div :key="index"
         v-for="(todo, index) in getTodos" 
@@ -49,5 +59,31 @@ export default {
   bottom: 1px;
   right: 5px;
   cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
